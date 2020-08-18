2641d2c446497be2c0eb3d0f2731956d3fe708a6
Stephen Enke <stephenenke@gmail.com>
2020-08-18 18:25:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-var number = 5
-console.log(number += 3)
+var number = 10
+function add5() {
+  number += 5
+}
+function divideBy3() {
+  number /= 3
+}
+divideBy3()
+console.log(number)
+add5()
+console.log(number)
+number = 10
+add5()
+console.log(number)
+divideBy3()
+console.log(number)
