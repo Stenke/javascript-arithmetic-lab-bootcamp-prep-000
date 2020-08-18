5f253505ce2dbceafadf44ffd2f4a311c7b69948
Stephen Enke <stephenenke@gmail.com>
2020-08-18 18:36:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 function add() {
   return "'add()' is a valid function"
 }
+function subtract() {
+  return "'subtract()' is a valid function"
+}
+function multiply() {
+  return "'multiply()' is a valid function"
+}
+function divide() {
+  return "'divide()' is a valid function"
+}
 function increment(n) {
   return n += 1
 }
