3fbf50215ee2651ab6c8c15eded2ee8439a1a010
Stephen Enke <stephenenke@gmail.com>
2020-08-18 18:28:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,16 +1,1 @@
-var number = 10
-function add5() {
-  number += 5
-}
-function divideBy3() {
-  number /= 3
-}
-divideBy3()
-console.log(number)
-add5()
-console.log(number)
-number = 10
-add5()
-console.log(number)
-divideBy3()
-console.log(number)
+console.log(parseInt('1.11', 10))
