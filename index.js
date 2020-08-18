1deb747df0f67cc41a80e9a62d9364dd09f5b994
Stephen Enke <stephenenke@gmail.com>
2020-08-18 18:39:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,6 +13,15 @@ function divide() {
 function add(a, b) {
   return a += b
 }
+function subtract(a, b) {
+  return a -= b
+}
+function multiply(a, b) {
+  return a *= b
+}
+function divide(a, b) {
+  return a += b
+}
 function increment(n) {
   return n += 1
 }
